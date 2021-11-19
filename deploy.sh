bc22e681d3c29066c1238cfa26a8a61c5be9d993
Nick Satsia <nsatsia@redhat.com>
2021-11-19 21:02:00 +1100

    .

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ echo -n "Waiting for OpenShift GitOps to commence installation."
 while ! `oc get crds| egrep argo -q`; do
 sleep $SLEEP
 echo -n "."
-done
 echo
 echo -n "Waiting 60 seconds for all CRDs to apply."
 for i in {1..60}; do echo -n "."; sleep 1; done
