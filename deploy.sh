166bc37bb6da4753563c3717a922a43bf735a323
Nick Satsia <nsatsia@redhat.com>
2021-11-19 21:10:01 +1100

    .

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
-SLEEP=3
 cd ztp/gitops-install
 helm install -f ../../values.yaml gitops-install .
 echo -n "Waiting for OpenShift GitOps to commence installation."
 while ! `oc get crds| egrep argo -q`; do
-sleep $SLEEP
+sleep 3
 echo -n "."
+done
 echo
 echo -n "Waiting 60 seconds for all CRDs to apply."
-for i in {1..60}; do echo -n "."; sleep 1; done
+for i in {1..60}; do
+  echo -n "."
+  sleep 1
+done
 echo
 cd ../gitops-config/
 helm install -f ../../values.yaml gitops-config . 
